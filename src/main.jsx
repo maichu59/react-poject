67e1d61862f4ac1e4d2943255c7db0f5a1f38915
maichu59 <withlove13441@gmail.com>
2025-08-13 15:42:32 +0800

    0813-HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './MyApp'
+
+
 // import './index.css'
 // import App from './App.jsx'
 // import './style.css'
-import './assets/all.scss'
-import App from './pages/App0813-weather-3'
+// import './assets/all.scss'
+
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   // 嚴謹模式StrictMode
-  <StrictMode>
+  <React.StrictMode>
+    <HashRouter>
     <App />
-  </StrictMode>,
+    </HashRouter>
+  </React.StrictMode>,
 )
