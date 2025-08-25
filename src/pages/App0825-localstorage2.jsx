56cf3293fdd16b12cc49d921520e4f2ab3f8e789
maichu59 <withlove13441@gmail.com>
2025-08-25 11:53:31 +0800

    0825-LocalStorage

---
+++ src/pages/App0825-localstorage2.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const App = () => {
+    const arrCitys = [
+        {
+            cityId: 1,
+            cityName: '台北市'
+        },
+        {
+            cityId: 2,
+            cityName: '桃園市'
+        },
+    ]
+
+
+    return (
+        <div>
+            <h2>Local storage - 陣列物件資料存取</h2>
+            <button onClick={()=>{
+                // 陣列物件轉字串
+                let strCity=JSON.stringify(arrCitys);
+
+                // 寫入local storage
+                window.localStorage.setItem('citys',strCity)
+            }}>寫入</button>
+
+            <button onClick={()=>{
+                // 取出local storage資料
+                let getData=window.localStorage.getItem('citys');
+                // 字串轉陣列物件
+                let getArrData=JSON.parse(getData);
+                console.log(getArrData);
+            }}>讀取</button>
+
+        </div>
+    )
+}
+
+
+export default App
